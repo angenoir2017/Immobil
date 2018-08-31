b913b1fe2b5c8864c72b129b7cae785bd19bb162
sem1999 <akadepremier60@gmail.com>
2018-08-31 17:47:03 +0000

    supression des import unitile;  changement du style du sides menu ;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,7 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import {ParametrePage} from "../pages/parametre/parametre";
-import {ComptePage} from "../pages/compte/compte";
+
 @Component({
   templateUrl: 'app.html'
 })
